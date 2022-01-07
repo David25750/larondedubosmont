f144d311631821515f8682f35694604205462766
David <david.philippe25750@gmail.com>
2022-01-07 01:46:34 +0100

    troisième sauvegarde

--- larondedubosmont/src/components/Resultats.vue
+++ larondedubosmont/src/components/Resultats.vue
@@ -75,99 +75,70 @@
         <div class="row">
           <p class="text-center text-white">Voir l'intégralité du classement</p>
         </div>
-        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center p-3"><a href="" download="https://parametre.davidphilippe.fr/wp-content/gallery/illustration_haut-de-page/angela-loria-ZAy8srF4rRM-unsplash.png">
+        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center p-3"><a href="#" download="#">
           <b-button
                     type="submit" class="btn btn-warning text-black px-5 gap-3">Classement intégrale</b-button></a>
-
         </div>
       </div>
-      <div class="container-fluid bg-white">
+      <div class="container bg-white">
         <div class="row">
           <h1 class="text-center text-uppercase display-5 my-5">Mentions honorable</h1>
         </div>
 
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 pb-4">
 
-          <div class="col">
+          <div class="col p-3">
             <div class="card border-0">
-              <a href="https://parametre.davidphilippe.fr/wp-content/uploads/2022/01/1c-Trace-Course-des-As-Ronde-du-Bosmont-Parcours-Danjoutin-2020-version-piste-cyclable.pdf" class="card text-center bg-success text-decoration-none">
+              <a href="https://parametre.davidphilippe.fr/wp-content/uploads/2022/01/1c-Trace-Course-des-As-Ronde-du-Bosmont-Parcours-Danjoutin-2020-version-piste-cyclable.pdf" class="card text-center bg-success text-center justify-content-center align-items-center text-decoration-none">
                 <div class="card-body align-items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="286.883" height="225" viewBox="0 0 286.883 278.268">
-                    <g id="graphisme_carte_parcous" data-name="graphisme carte parcous" transform="translate(-254.838 -4368.563)">
-                      <path id="Tracé_45" data-name="Tracé 45" d="M267.694,4059.74l57.4,24.351,49.816-24.351,47.651,21.229,14.079,59.316,88.262-4.371L369.493,4296.379l-4.332-89.286-78.515,8.117Z" transform="matrix(0.996, 0.087, -0.087, 0.996, 366.123, 314.652)" fill="none" stroke="#000" stroke-width="5"/>
-                      <g id="Ellipse_8" data-name="Ellipse 8" transform="matrix(0.996, 0.087, -0.087, 0.996, 268.43, 4368.563)" fill="#297319" stroke="#000" stroke-width="5">
-                        <circle cx="16" cy="16" r="16" stroke="none"/>
-                        <circle cx="16" cy="16" r="13.5" fill="none"/>
-                      </g>
-                      <g id="Ellipse_9" data-name="Ellipse 9" transform="matrix(0.996, 0.087, -0.087, 0.996, 277.433, 4380.394)" fill="#2e2828" stroke="#000" stroke-width="1">
-                        <ellipse cx="6" cy="5.5" rx="6" ry="5.5" stroke="none"/>
-                        <ellipse cx="6" cy="5.5" rx="5.5" ry="5" fill="none"/>
-                      </g>
-                      <g id="Ellipse_10" data-name="Ellipse 10" transform="matrix(0.996, 0.087, -0.087, 0.996, 428.378, 4441.28)" fill="#297319" stroke="#000" stroke-width="4">
-                        <ellipse cx="7" cy="6.5" rx="7" ry="6.5" stroke="none"/>
-                        <ellipse cx="7" cy="6.5" rx="5" ry="4.5" fill="none"/>
-                      </g>
-                      <g id="Ellipse_11" data-name="Ellipse 11" transform="matrix(0.996, 0.087, -0.087, 0.996, 379.314, 4384.288)" fill="#297319" stroke="#000" stroke-width="4">
-                        <ellipse cx="7" cy="6.5" rx="7" ry="6.5" stroke="none"/>
-                        <ellipse cx="7" cy="6.5" rx="5" ry="4.5" fill="none"/>
-                      </g>
-                      <g id="Ellipse_12" data-name="Ellipse 12" transform="matrix(0.996, 0.087, -0.087, 0.996, 480.045, 4470.967)" fill="#297319" stroke="#000" stroke-width="4">
-                        <ellipse cx="7" cy="6.5" rx="7" ry="6.5" stroke="none"/>
-                        <ellipse cx="7" cy="6.5" rx="5" ry="4.5" fill="none"/>
-                      </g>
-                      <g id="Ellipse_13" data-name="Ellipse 13" transform="translate(493.991 4498.353)" fill="#297319" stroke="#000" stroke-width="4">
-                        <ellipse cx="7" cy="6.5" rx="7" ry="6.5" stroke="none"/>
-                        <ellipse cx="7" cy="6.5" rx="5" ry="4.5" fill="none"/>
-                      </g>
-                      <g id="Ellipse_14" data-name="Ellipse 14" transform="matrix(0.996, 0.087, -0.087, 0.996, 425.402, 4558.466)" fill="#297319" stroke="#000" stroke-width="4">
-                        <ellipse cx="7" cy="6.5" rx="7" ry="6.5" stroke="none"/>
-                        <ellipse cx="7" cy="6.5" rx="5" ry="4.5" fill="none"/>
-                      </g>
-                      <g id="Ellipse_15" data-name="Ellipse 15" transform="matrix(0.996, 0.087, -0.087, 0.996, 353.098, 4622.157)" fill="#297319" stroke="#000" stroke-width="4">
-                        <ellipse cx="7" cy="6.5" rx="7" ry="6.5" stroke="none"/>
-                        <ellipse cx="7" cy="6.5" rx="5" ry="4.5" fill="none"/>
-                      </g>
-                      <g id="Ellipse_16" data-name="Ellipse 16" transform="matrix(0.996, 0.087, -0.087, 0.996, 356.098, 4530.831)" fill="#297319" stroke="#000" stroke-width="4">
-                        <ellipse cx="7" cy="6.5" rx="7" ry="6.5" stroke="none"/>
-                        <ellipse cx="7" cy="6.5" rx="5" ry="4.5" fill="none"/>
-                      </g>
-                      <g id="Ellipse_17" data-name="Ellipse 17" transform="matrix(0.996, 0.087, -0.087, 0.996, 279.184, 4532.49)" fill="#297319" stroke="#000" stroke-width="4">
-                        <ellipse cx="7" cy="6.5" rx="7" ry="6.5" stroke="none"/>
-                        <ellipse cx="7" cy="6.5" rx="5" ry="4.5" fill="none"/>
-                      </g>
-                      <g id="Ellipse_18" data-name="Ellipse 18" transform="matrix(0.996, 0.087, -0.087, 0.996, 273.725, 4445.727)" fill="#297319" stroke="#000" stroke-width="4">
-                        <ellipse cx="7" cy="6.5" rx="7" ry="6.5" stroke="none"/>
-                        <ellipse cx="7" cy="6.5" rx="5" ry="4.5" fill="none"/>
-                      </g>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="65" height="150" viewBox="0 0 358.622 192.481">
+                    <g id="Rectangle_41" data-name="Rectangle 41" transform="translate(358.622) rotate(90)" fill="#297319" stroke="rgba(112,112,112,0)" stroke-width="1">
+                      <rect width="192.481" height="358.622" stroke="none"/>
+                      <rect x="0.5" y="0.5" width="191.481" height="357.622" fill="none"/>
                     </g>
                   </svg>
                 </div>
-                <h4 class="card-text text-center text-uppercase bg-dark text-white py-2">ANDREY Marie-Laure</h4>
+                <h2 class="position-absolute di text-uppercase  text-white">ANDREY Marie-Laure</h2>
+                <h4 class="card-title text-uppercase text-white"><strong>Meilleur femme</strong></h4>
               </a>
             </div>
           </div>
 
-          <div class="col px-5">
-            <h2 class="lead text-center bg-success p-5">ANDREY Marie-Laure</h2>
+          <div class="col p-3">
+            <div class="card border-0">
+              <a href="https://parametre.davidphilippe.fr/wp-content/uploads/2022/01/1c-Trace-Course-des-As-Ronde-du-Bosmont-Parcours-Danjoutin-2020-version-piste-cyclable.pdf" class="card text-center bg-success text-center justify-content-center align-items-center text-decoration-none">
+                <div class="card-body align-items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="65" height="150" viewBox="0 0 358.622 192.481">
+                    <g id="Rectangle_41" data-name="Rectangle 41" transform="translate(358.622) rotate(90)" fill="#297319" stroke="rgba(112,112,112,0)" stroke-width="1">
+                      <rect width="192.481" height="358.622" stroke="none"/>
+                      <rect x="0.5" y="0.5" width="191.481" height="357.622" fill="none"/>
+                    </g>
+                  </svg>
+                </div>
+                <h2 class="position-absolute di text-uppercase  text-white">LE BON Marc</h2>
+                <h4 class="card-title text-uppercase text-white"><strong>Doyen de la course</strong></h4>
+              </a>
+            </div>
           </div>
-          <div class="col px-5">
-            <h2 class="lead text-center bg-success p-5">LE BON Marc</h2>
-          </div>
-          <div class="col px-5">
-            <h2 class="lead text-center bg-success p-5">BELFORT ATHLÉ</h2>
+
+          <div class="col p-3">
+            <div class="card border-0">
+              <a href="https://parametre.davidphilippe.fr/wp-content/uploads/2022/01/1c-Trace-Course-des-As-Ronde-du-Bosmont-Parcours-Danjoutin-2020-version-piste-cyclable.pdf" class="card text-center bg-success text-center justify-content-center align-items-center text-decoration-none">
+                <div class="card-body align-items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="65" height="150" viewBox="0 0 358.622 192.481">
+                    <g id="Rectangle_41" data-name="Rectangle 41" transform="translate(358.622) rotate(90)" fill="#297319" stroke="rgba(112,112,112,0)" stroke-width="1">
+                      <rect width="192.481" height="358.622" stroke="none"/>
+                      <rect x="0.5" y="0.5" width="191.481" height="357.622" fill="none"/>
+                    </g>
+                  </svg>
+                </div>
+                <h2 class="position-absolute di text-uppercase  text-white">BELFORT ATHLÉ</h2>
+                <h4 class="card-title text-uppercase text-white"><strong>Meilleur Club</strong></h4>
+              </a>
+            </div>
           </div>
         </div>
-        <div class="row row-cols-3 mx-5 pb-5">
-          <div class="col px-5">
-            <h2 class="lead text-center bg-dark text-white p-2"><strong>Meilleur femme</strong></h2>
-          </div>
-          <div class="col px-5">
-            <h2 class="lead text-center bg-dark text-white p-2"><strong>Doyen de la course</strong></h2>
-          </div>
-          <div class="col px-5">
-            <h2 class="lead text-center bg-dark text-white p-2"><strong>Meilleur Club</strong></h2>
-          </div>
-        </div>
+
       </div>
 
 
